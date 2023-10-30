461a45c34b1625bffd48d0c602d547cf460edfd5
gonçalo Pereira <128597095+sardinhaa@users.noreply.github.com>
2023-10-30 09:07:25 -0100

    Update teste1.sh

--- teste1.sh
+++ teste1.sh
@@ -5,13 +5,13 @@ rpt="s"
 while [ "$rpt" = "s" ]; do
     for i in {1..5}; do
         echo "aqui esta a chave:"
-        echo -n "Números: "
+        echo -n "numeros: "
         for j in {1..5}; do
             numero=$((1 + RANDOM % 50))
             echo -n "$numero "
         done
 
-        echo -n "Estrelas: "
+        echo -n "estrelas: "
         for k in {1..2}; do
             estrelas=$((1 + RANDOM % 12))
             echo -n "$estrelas "
