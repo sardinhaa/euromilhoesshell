0006f10a39d55f69a9028515adb88424b4c75342
gonçalo Pereira <128597095+sardinhaa@users.noreply.github.com>
2023-10-31 08:57:53 -0100

    Update teste.sh

--- teste.sh
+++ teste.sh
@@ -2,33 +2,35 @@
 
 rpt="s"
 
+while [ "$rpt" = "s" ]; do
+    echo "aqui esta a chave:"
+    
+    numbers=()
+    stars=()
+    
+    echo -n "numeros: "
+    while [ ${#numbers[@]} -lt 5 ]; do
+        numero=$((RANDOM % 50 + 1))
+        
+        if ! [[ " ${numbers[*]} " =~ " $numero " ]]; then
+            numbers+=($numero)
+            echo -n "$numero "
+        fi
+    done
 
-while [[ " ${numeros[@]} " =~ " $randomNum " ]]; do
-       echo "Número $randomNum já existe no intervalo 1-12. Gerando outro número."
-       randomNum=$((RANDOM%12+1))
-       echo "Novo número gerado: $randomNum"
-   done
-   numeros_1_12+=("$randomNum")
-   echo "$randomNum" >> "$ficheiro"
-   contador=$(($contador+1))
-        done
+    echo -n "estrelas: "
+    while [ ${#stars[@]} -lt 2 ]; do
+        estrelas=$((RANDOM % 12 + 1))
+        
+        if ! [[ " ${stars[*]} " =~ " $estrelas " ]]; then
+            stars+=($estrelas)
+            echo -n "$estrelas "
+        fi
+    done
 
-while [[ " ${estrelas[@]} " =~ " $randomNum " ]]; do
-       echo "Número $randomNum já existe no intervalo 1-12. Gerando outro número."
-       randomNum=$((RANDOM%12+1))
-       echo "Novo número gerado: $randomNum"
-   done
-   numeros_1_12+=("$randomNum")
-   echo "$randomNum" >> "$ficheiro"
-   contador=$(($contador+1))
-        done
-
-        echo
+    echo
 
     read -p "queres gerar outra chave para o euromilhoes? (s/n): " rpt
 done
 
 echo "adeus"
-
-
-
