6c40fe75c07f75cbef8194715880f516cdc2b3d1
gonçalo Pereira <128597095+sardinhaa@users.noreply.github.com>
2023-10-30 23:28:35 -0100

    Update teste1.sh

--- teste1.sh
+++ teste1.sh
@@ -1,29 +1,24 @@
 #!/bin/bash
 
-rpt="s"
+while true; do
+    echo "Chave do EuroMilhões:"
 
-while [ "$rpt" = "s" ]; do
-        echo "aqui esta a chave:"
-        echo -n "numeros: "
-        for ((i = 1; i <= 5; i++)); ger50 in {1..5}; do
-            numero=$((RANDOM % 50 + 1))
-            echo -n "$numero "
-        done
+    echo -n "Números: "
+    for ((i = 1; i <= 5; i++)); do
+    numero=$(( RANDOM % 50 + 1))
+    echo -n "$numero "
+    done
 
-        echo -n "estrelas: "
-        for ((i = 1; i <= 2; i++)); ger12 in {1..2}; do
-            estrelas=$((RANDOM % 12 + 1))
-            echo -n "$estrelas "
-        done
+    echo -n "Estrelas: "
+    for ((i = 1; i <= 2; i++)); do
+    estrela=$((1 + RANDOM % 12))
+    echo -n "$estrela "
+    done
 
-        echo
+    echo
 
-    read -p "queres gerar outra chave para o euromilhoes? (s/n): " rpt
-done
-
-while [ "$rpt" = "n" ];do 
-    echo "adeus" 
+    read -p "Deseja gerar outra chave? (S para sim, qualquer tecla para sair): " resposta
+    if [ "$resposta" != "S" ] && [ "$resposta" != "s" ]; then
     break
+    fi
 done
-
-
