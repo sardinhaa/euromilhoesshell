ce054ad963e621a2c71a547b3322696622e4c047
gonçalo Pereira <128597095+sardinhaa@users.noreply.github.com>
2023-10-31 08:49:35 -0100

    Update teste.sh

--- teste.sh
+++ teste.sh
@@ -2,18 +2,25 @@
 
 rpt="s"
 
-while [ "$rpt" = "s" ]; do
-        echo "aqui esta a chave:"
-        echo -n "numeros: "
-        for ((i = 1; i <= 5; i++)); do
-            numero=$((RANDOM % 50 + 1))
-            echo -n "$numero "
+
+while [[ " ${numeros[@]} " =~ " $randomNum " ]]; do
+       echo "Número $randomNum já existe no intervalo 1-12. Gerando outro número."
+       randomNum=$((RANDOM%12+1))
+       echo "Novo número gerado: $randomNum"
+   done
+   numeros_1_12+=("$randomNum")
+   echo "$randomNum" >> "$ficheiro"
+   contador=$(($contador+1))
         done
 
-        echo -n "estrelas: "
-        for ((i = 1; i <= 2; i++)); do
-            estrelas=$((RANDOM % 12 + 1))
-            echo -n "$estrelas "
+while [[ " ${estrelas[@]} " =~ " $randomNum " ]]; do
+       echo "Número $randomNum já existe no intervalo 1-12. Gerando outro número."
+       randomNum=$((RANDOM%12+1))
+       echo "Novo número gerado: $randomNum"
+   done
+   numeros_1_12+=("$randomNum")
+   echo "$randomNum" >> "$ficheiro"
+   contador=$(($contador+1))
         done
 
         echo
@@ -23,13 +30,5 @@ done
 
 echo "adeus"
 
-while [[ " ${numeros_1_12[@]} " =~ " $randomNum " ]]; do
-       echo "Número $randomNum já existe no intervalo 1-12. Gerando outro número."
-       randomNum=$((RANDOM%12+1))
-       echo "Novo número gerado: $randomNum"
-   done
-   numeros_1_12+=("$randomNum")
-   echo "$randomNum" >> "$ficheiro"
-   contador=$(($contador+1))
 
 
